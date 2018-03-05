9216a4ad87ba94a8a476c58bf102cd6e761d7643
mhortillano <mhortillano@github.com>
2018-03-05 09:53:09 -1000

    Completed HackWhichSix

--- HackWhichSix/ViewController.swift
+++ HackWhichSix/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var slider: UISlider!
+    //create a variable that can hold the value of the slider
+    var currentValue: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,31 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func SliderHasMoved(_ sender: Any) {
+        print("The Value of the slider is:\(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func MyGuessButtonPressed(_ sender: Any) {
+        
+        //5. New Variable message that displays a message that includes currentValue
+        let message = "The Value is: \(currentValue)"
+        
+        //1. Create alert View
+        let alert = UIAlertController(title: "Hello World!", message: message, preferredStyle: .alert)
+        
+        //2. Button that user taps to dismuss view controller
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        //3. Add the button to the alertview
+        alert.addAction(action)
+        
+        //4. present alertview on the screen
+        present(alert, animated: true, completion: nil)
+        
+       
+    }
+    
 }
 
