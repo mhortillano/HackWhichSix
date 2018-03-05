cb0212df4918d27bae0de55ee26ec13e1eedaef2
mhortillano <mhortillano@github.com>
2018-03-05 09:21:41 -1000

    Initial Commit

---
+++ HackWhichSix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackWhichSix
+//
+//  Created by Mariah Hortillano on 3/5/18.
+//  Copyright © 2018 Mariah Hortillano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
